22d5e676e6dc7ec21e98545c7cbb4c94236c68ff
Andrea <acesparza05@gmail.com>
2022-07-06 10:51:10 -0500

    finished assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        buildList(e.target['new-task-description'].value)
+        form.reset()
+    })
 });
+
+function buildList(item) {
+    const pList = document.createElement('p')
+    const btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    pList.textContent = `${item}    `
+    pList.appendChild(btn)
+    document.querySelector('#tasks').appendChild(pList)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
